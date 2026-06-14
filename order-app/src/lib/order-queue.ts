b6715530723258e191e127a52298389d1f849669
Wei-Lenovo <tsuweiapptest@gmail.com>
2026-06-14 15:16:09 +0800

    - added order-app

---
+++ order-app/src/lib/order-queue.ts
@@ -0,0 +1,34 @@
+import { Order, OrderType } from "@/lib/types";
+
+export class OrderQueue {
+  private vipQueue: Order[] = [];
+  private normalQueue: Order[] = [];
+
+  enqueue(order: Order) {
+    if (order.type === OrderType.VIP) {
+      this.vipQueue.push(order);
+    } else {
+      this.normalQueue.push(order);
+    }
+  }
+
+  dequeue(): Order | undefined {
+    if (this.vipQueue.length > 0) {
+      return this.vipQueue.shift();
+    }
+
+    return this.normalQueue.shift();
+  }
+
+  requeue(order: Order) {
+    if (order.type === OrderType.VIP) {
+      this.vipQueue.unshift(order);
+    } else {
+      this.normalQueue.unshift(order);
+    }
+  }
+
+  getPending() {
+    return [...this.vipQueue, ...this.normalQueue];
+  }
+}
